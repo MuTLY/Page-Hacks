2aec7b91a38bd376ee447b089832b3eb05715f04
Leandro Barbosa <mutly@live.com>
2019-05-28 12:19:27 -0300

    Create IcyVeins-Wowdb2Wowhead.js

---
+++ IcyVeins-Wowdb2Wowhead.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Icy Veins Wowdb -> Wowhead
+// @namespace    https://www.icy-veins.com/
+// @version      1.0
+// @description  Switch Wowdb links with Wowhead links on Icy Veins
+// @author       MuTLY
+// @match        https://www.icy-veins.com/wow/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var links,
+        thisLink;
+    links = document.evaluate("//a[@href]", document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+    for (var i = 0; i < links.snapshotLength; i++) {
+        thisLink = links.snapshotItem(i);
+        thisLink.href = thisLink.href.replace('https://www.wowdb.com/items/', 'https://www.wowhead.com/item=');
+    }
+})();
